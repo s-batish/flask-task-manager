3bb98db0ebeb744622713cd72509d46737f0dc55
s-batish <>
2023-05-19 12:36:08 +0000

    Add template for users to view tasks

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -5,7 +5,8 @@ from taskmanager.models import Category, Task
 
 @app.route("/")
 def home():
-    return render_template("tasks.html")
+    tasks = list(Task.query.order_by(Task.id).all())
+    return render_template("tasks.html", tasks=tasks)
 
 
 @app.route("/categories")
@@ -55,5 +56,5 @@ def add_task():
         )
         db.session.add(task)
         db.session.commit()
-        return redirect(url_for("home"))
+    return render_template("add_task.html", categories=categories)
     return render_template("add_task.html", categories=categories)
